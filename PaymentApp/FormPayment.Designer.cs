a26ef6dc7cca97dd1a1adf5756c4510c9f1fd57a
Connor Dufault <mx8434an@go.minnstate.edu>
2020-04-06 15:57:11 -0500

    added payment app

---
+++ PaymentApp/FormPayment.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace PaymentApp
+{
+    partial class FormPayment
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rdoBillCustomer = new System.Windows.Forms.RadioButton();
+            this.rdoCreditCard = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lstCreditCardType = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtCardNumber = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cboMonth = new System.Windows.Forms.ComboBox();
+            this.cboYear = new System.Windows.Forms.ComboBox();
+            this.chkDefault = new System.Windows.Forms.CheckBox();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rdoBillCustomer);
+            this.groupBox1.Controls.Add(this.rdoCreditCard);
+            this.groupBox1.Location = new System.Drawing.Point(24, 31);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(689, 128);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Billing";
+            // 
+            // rdoBillCustomer
+            // 
+            this.rdoBillCustomer.AutoSize = true;
+            this.rdoBillCustomer.Location = new System.Drawing.Point(372, 56);
+            this.rdoBillCustomer.Name = "rdoBillCustomer";
+            this.rdoBillCustomer.Size = new System.Drawing.Size(166, 29);
+            this.rdoBillCustomer.TabIndex = 1;
+            this.rdoBillCustomer.Text = "Bill customer";
+            this.rdoBillCustomer.UseVisualStyleBackColor = true;
+            // 
+            // rdoCreditCard
+            // 
+            this.rdoCreditCard.AutoSize = true;
+            this.rdoCreditCard.Checked = true;
+            this.rdoCreditCard.Location = new System.Drawing.Point(45, 56);
+            this.rdoCreditCard.Name = "rdoCreditCard";
+            this.rdoCreditCard.Size = new System.Drawing.Size(152, 29);
+            this.rdoCreditCard.TabIndex = 0;
+            this.rdoCreditCard.TabStop = true;
+            this.rdoCreditCard.Text = "Credit Card";
+            this.rdoCreditCard.UseVisualStyleBackColor = true;
+            this.rdoCreditCard.CheckedChanged += new System.EventHandler(this.rdoCreditCard_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 185);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(170, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Credit card type:";
+            // 
+            // lstCreditCardType
+            // 
+            this.lstCreditCardType.FormattingEnabled = true;
+            this.lstCreditCardType.ItemHeight = 25;
+            this.lstCreditCardType.Location = new System.Drawing.Point(245, 185);
+            this.lstCreditCardType.Name = "lstCreditCardType";
+            this.lstCreditCardType.Size = new System.Drawing.Size(468, 154);
+            this.lstCreditCardType.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 370);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(145, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Card Number:";
+            // 
+            // txtCardNumber
+            // 
+            this.txtCardNumber.Location = new System.Drawing.Point(245, 367);
+            this.txtCardNumber.Name = "txtCardNumber";
+            this.txtCardNumber.Size = new System.Drawing.Size(468, 31);
+            this.txtCardNumber.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(34, 439);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(162, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Expiration date:";
+            // 
+            // cboMonth
+            // 
+            this.cboMonth.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboMonth.FormattingEnabled = true;
+            this.cboMonth.Location = new System.Drawing.Point(245, 436);
+            this.cboMonth.Name = "cboMonth";
+            this.cboMonth.Size = new System.Drawing.Size(214, 33);
+            this.cboMonth.TabIndex = 6;
+            // 
+            // cboYear
+            // 
+            this.cboYear.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboYear.FormattingEnabled = true;
+            this.cboYear.Location = new System.Drawing.Point(485, 436);
+            this.cboYear.Name = "cboYear";
+            this.cboYear.Size = new System.Drawing.Size(228, 33);
+            this.cboYear.TabIndex = 7;
+            // 
+            // chkDefault
+            // 
+            this.chkDefault.AutoSize = true;
+            this.chkDefault.Checked = true;
+            this.chkDefault.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkDefault.Location = new System.Drawing.Point(39, 523);
+            this.chkDefault.Name = "chkDefault";
+            this.chkDefault.Size = new System.Drawing.Size(315, 29);
+            this.chkDefault.TabIndex = 8;
+            this.chkDefault.Text = "Set as default billing method";
+            this.chkDefault.UseVisualStyleBackColor = true;
+            // 
+            // btnOK
+            // 
+            this.btnOK.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnOK.Location = new System.Drawing.Point(418, 596);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(138, 52);
+            this.btnOK.TabIndex = 9;
+            this.btnOK.Text = "Ok";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(575, 596);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(138, 52);
+            this.btnCancel.TabIndex = 10;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // FormPayment
+            // 
+            this.AcceptButton = this.btnOK;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnCancel;
+            this.ClientSize = new System.Drawing.Size(748, 686);
+            this.ControlBox = false;
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.chkDefault);
+            this.Controls.Add(this.cboYear);
+            this.Controls.Add(this.cboMonth);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtCardNumber);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lstCreditCardType);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "FormPayment";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Payment";
+            this.Load += new System.EventHandler(this.FormPayment_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rdoBillCustomer;
+        private System.Windows.Forms.RadioButton rdoCreditCard;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox lstCreditCardType;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtCardNumber;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cboMonth;
+        private System.Windows.Forms.ComboBox cboYear;
+        private System.Windows.Forms.CheckBox chkDefault;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
---
+++ PaymentApp/FormPayment.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+
+namespace PaymentApp
+{
+    public partial class FormPayment : Form
+    {
+        public FormPayment()
+        {
+            InitializeComponent();
+        }
+
+        private void FormPayment_Load(object sender, EventArgs e)
+        {
+            lstCreditCardType.Items.Add("Visa");
+            lstCreditCardType.Items.Add("MasterCard");
+            lstCreditCardType.Items.Add("American Express");
+            lstCreditCardType.SelectedIndex = 0;
+
+            string[] months = {"Select a month...", "January", "February", "March", "April", "May", "June",
+            "July", "August", "September", "October", "November", "December"};
+            cboMonth.Items.AddRange(months);
+            cboMonth.SelectedIndex = 0;
+
+            int year = DateTime.Today.Year;
+            int endYear = year + 8;
+            cboYear.Items.Add("Select a year...");
+            while (year < endYear)
+            {
+                cboYear.Items.Add(year);
+                year++;
+            }
+            cboYear.SelectedIndex = 0;
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            if (IsValidData())
+            {
+                SaveData();
+                DialogResult = DialogResult.OK;
+            }
+        }
+
+        private bool IsValidData()
+        {
+            if (rdoCreditCard.Checked)
+            {
+                if(lstCreditCardType.SelectedIndex == -1)
+                {
+                    MessageBox.Show("Select a credit card type", "Error");
+                    lstCreditCardType.Focus();
+                    return false;
+                }
+
+                // check that there is a card number inputted and that it's a number
+                if (String.IsNullOrEmpty(txtCardNumber.Text) || !int.TryParse(txtCardNumber.Text, out int result))
+                {
+                    MessageBox.Show("Enter a valid credit card number", "Error");
+                    txtCardNumber.Focus();
+                    return false;
+                }
+
+                if (cboMonth.SelectedIndex == 0)
+                {
+                    MessageBox.Show("Select a month", "Error");
+                    cboMonth.Focus();
+                    return false;
+                }
+
+                if (cboYear.SelectedIndex == 0)
+                {
+                    MessageBox.Show("Select a year", "Error");
+                    cboYear.Focus();
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private void SaveData()
+        {
+            // build a string with the customer's billing info
+            StringBuilder billingDataBuilder = new StringBuilder();
+
+            if (rdoCreditCard.Checked)
+            {
+                billingDataBuilder.Append("Charge to credit card.\n\n");
+                billingDataBuilder.Append($"Card type: {lstCreditCardType.Text}\n");
+                billingDataBuilder.Append($"Card number: {txtCardNumber.Text}\n");
+                billingDataBuilder.Append($"Expiration date: {cboMonth.SelectedItem}/{cboYear.SelectedItem}\n");
+            }
+            else
+            {
+                billingDataBuilder.Append("Send bill to customer.\n\n");
+            }
+
+            bool makeDefault = chkDefault.Checked;
+            billingDataBuilder.Append($"Default billing method: {makeDefault}");
+
+            string billingData = billingDataBuilder.ToString();
+            Tag = billingData;
+        }
+
+        private void rdoCreditCard_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rdoCreditCard.Checked)
+            {
+                EnableControls();
+            }
+            else
+            {
+                DisableControls();
+            }
+        }
+
+        private void EnableControls()
+        {
+            lstCreditCardType.Enabled = true;
+            txtCardNumber.Enabled = true;
+            cboMonth.Enabled = true;
+            cboYear.Enabled = true;
+        }
+
+        private void DisableControls()
+        {
+            lstCreditCardType.Enabled = false;
+            txtCardNumber.Enabled = false;
+            cboMonth.Enabled = false;
+            cboYear.Enabled = false;
+        }
+    }
+}
